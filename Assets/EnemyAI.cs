0900aba8a23db344d2bf57bd7f1c9f85ed60f8f2
rdavis223 <44733603+rdavis223@users.noreply.github.com>
2021-10-29 06:29:28 -0400

    Basic attack movement

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -8,35 +8,123 @@ public class EnemyAI : MonoBehaviour
     private bool destSet;
     private Vector3 dest;
     public float speed;
+    private Quaternion dir;
+    public float turnSpeed = 1f;
+
+    public GameObject player;
+    public float playerAttackRange = 600f;
+    public bool dodging = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Patrol();
+        if (distanceToPlayer() < playerAttackRange)
+        {
+            destSet = false;
+            attackPlayer();
+        }
+        else
+        {
+            Patrol();
+        }
+        
     }
 
     void Patrol()
     {
         if (!destSet)
         {
-            dest = new Vector3(Random.Range(-range, range), Random.Range(-range, range), Random.Range(-range, range));
+            dest = new Vector3(Random.Range(0, range), Random.Range(0, range), Random.Range(0, range));
             destSet = true;
+            dir = Quaternion.LookRotation(dest - this.transform.position);
+
         }
-        if (Vector3.Distance(this.transform.position, dest) <= 1f)
+        if (isAtPoint(this.transform.position, dest))
         {
             destSet = false;
         }
         else
         {
-            this.transform.LookAt(dest);
-            this.transform.position += this.transform.forward * speed * Time.deltaTime;
+            turnTowardsVector(dir);
+            flyInDirection(dest - this.transform.position);
         }
     }
+
+    void attackPlayer()
+    {
+        if (dodging)
+        {
+            dodgePlayer();
+        }
+        else
+        {
+            flyTowardsPlayer();
+        }
+    }
+
+    void flyTowardsPlayer()
+    {
+        Vector3 playerDir = (player.transform.position - this.transform.position).normalized;
+        Quaternion playerRot = Quaternion.LookRotation(player.transform.position - this.transform.position);
+        turnTowardsVector(playerRot);
+        if (distanceToPlayer() > 30f)
+        {
+            flyInDirection(playerDir);
+        }
+        else
+        {
+            Vector3 newDirection = Quaternion.Euler(Random.Range(30f, 50f), 0f, 0f) * playerDir;
+            Debug.Log(this.transform.position);
+            dest = this.transform.position + (newDirection.normalized * Random.Range(150f, 400f));
+            Debug.Log(dest);
+            dir = Quaternion.LookRotation(newDirection);
+            dodging = true;
+        }
+    }
+
+    void dodgePlayer()
+    {
+        turnTowardsVector(dir);
+        flyTowardsPoint(dest);
+        if (isAtPoint(this.transform.position, dest))
+        {
+            dodging = false;
+        }
+    }
+    void turnTowardsVector(Quaternion rot)
+    {
+        if (this.transform.rotation != rot)
+        {
+            this.transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+        }
+    }
+
+    void flyInDirection(Vector3 point)
+    {
+        this.transform.position += point.normalized * speed * Time.deltaTime;
+
+    }
+
+    void flyTowardsPoint(Vector3 point)
+    {
+        flyInDirection(point - this.transform.position);
+    }
+
+    float distanceToPlayer()
+    {
+        return Vector3.Distance(this.transform.position, player.transform.position);
+    }
+
+    bool isAtPoint(Vector3 pos, Vector3 destination)
+    {
+        return Vector3.Distance(this.transform.position, dest) <= 1f;
+    }
+
 
 }
