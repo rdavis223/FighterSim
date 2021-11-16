9cf6fb7a0c50a157e183640d248d616fff414018
rdavis223 <44733603+rdavis223@users.noreply.github.com>
2021-11-15 22:49:37 -0500

    added random scaling for asteroids

--- Assets/Scripts/AstroidSpawner.cs
+++ Assets/Scripts/AstroidSpawner.cs
@@ -15,8 +15,9 @@ public class AstroidSpawner : MonoBehaviour
         int i = 0;
         while (i < asteroidsToSpawn)
         {
+            float size = Random.Range(1f, 3f);
             Vector3 pos = new Vector3(Random.Range(0f, 1500f), Random.Range(0f, 1500f), Random.Range(0f, 1500f));
-            Collider[] hitColliders = Physics.OverlapSphere(pos, 5f);
+            Collider[] hitColliders = Physics.OverlapSphere(pos, 5f * size);
             if (hitColliders.Length == 0)
             {
                 GameObject asteroid;
@@ -29,6 +30,8 @@ public class AstroidSpawner : MonoBehaviour
                     asteroid = Instantiate(asteroidBlue);
                 }
                 asteroid.transform.position = pos;
+                float currentScale = asteroid.transform.localScale.x;
+                asteroid.transform.localScale = new Vector3(size*currentScale, size*currentScale, size * currentScale);
                 i++;
             }
         } 
